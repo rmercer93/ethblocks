0d866220d8497c252ff2f1bf5bbf85b9fa581a18
rhm <ucabrhm@ucl.ac.uk>
2017-06-07 00:44:44 +0100

    scripts and plots

---
+++ scripts/calctimes.py
@@ -0,0 +1,19 @@
+import csv
+import numpy as np
+import matplotlib.mlab as mlab
+import matplotlib.pyplot as plt
+
+with open('unixtime.csv', 'rb') as f:
+    a = np.loadtxt(f, dtype='int64')
+
+diffs = np.diff(a)
+i = np.where(diffs == 13013)
+diffs = np.delete(diffs, i)
+y = np.bincount(diffs)
+ii = np.nonzero(y)[0]
+
+data = zip(ii, y[ii])
+with open('diffs.csv', 'wb') as f:
+    writer = csv.writer(f)
+    for word in diffs:
+            writer.writerow([word])
---
+++ scripts/dist.R
@@ -0,0 +1,11 @@
+#!/usr/bin/env Rscript
+
+# leave the silly outlier out for better plots
+
+blocktimes = as.matrix(read.csv(file = "./data/diffs.csv", head = FALSE))
+blocktimes = setNames(blocktimes, c("blocktime"))
+summary(blocktimes)
+quantile(blocktimes, c(0.9, 0.95, 0.99))
+png(file = "mygraphic.png",width = 1000,height = 350)
+hist(blocktimes, breaks = 1000, freq = FALSE)
+dev.off()
