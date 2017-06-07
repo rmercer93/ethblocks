9ddfff38a8c937fb0915b32cd0aaaadc633112c1
rhm <ucabrhm@ucl.ac.uk>
2017-06-07 10:38:58 +0100

    more quartiles!!!;

--- scripts/dist.R
+++ scripts/dist.R
@@ -5,7 +5,7 @@
 blocktimes = as.matrix(read.csv(file = "../data/diffs.csv", head = FALSE))
 blocktimes = setNames(blocktimes, c("blocktime"))
 summary(blocktimes)
-quantile(blocktimes, c(0.9, 0.95, 0.99))
+quantile(blocktimes, c(0.9, 0.95, 0.99, 0.999, 0.9999))
 png(file = "blocktimedistro.png",width = 1000,height = 350)
 hist(blocktimes, breaks = 600, freq = FALSE)
 dev.off()
